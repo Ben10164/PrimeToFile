2d8151490e277537c8490d473019410e93770fb4
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2019-11-10 16:45:10 -0800

    Added a new program
    Added a new program that will count the lines of numbers in the prime file. This can be helpful if the computer used does not have a large text file editor so you can not see the number of lines.

---
+++ PrimesCount/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Primes_Count
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Press any button to start the process of counting the numbers in the primes document!");
+            Console.ReadKey();
+            Console.WriteLine("Now starting!\nIt can take a while so be prepared to wait if the file is large.");
+
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fullDocPath = Path.Combine(docPath, "primes.txt");
+
+            UInt64 counter = 0;
+            if (File.Exists(fullDocPath))
+            {
+                using (StreamReader sr = File.OpenText(fullDocPath))
+                {
+                    string s = String.Empty;
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        counter++;
+                    }
+                }
+                Console.WriteLine(counter);
+                Console.WriteLine("Keep in mind this only counts the lines.");
+            }
+            else
+            {
+                Console.WriteLine("Hey so, the file doesn't exist. It should be located at " + fullDocPath + " so if you moved it, neither programs can work.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
