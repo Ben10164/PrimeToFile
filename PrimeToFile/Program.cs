a75b5a5022df3b98e1283840eda6a9c11b957a30
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2019-12-11 06:02:33 -0800

    updated

--- PrimeToFile/Program.cs
+++ PrimeToFile/Program.cs
@@ -7,28 +7,82 @@ namespace PrimeToFile
 {
     internal class Program
     {
-        private const int MaxArrSize = 134212856/2;
+        private const int MaxArrSize = 100000000 / 2;
 
         public static void ConvertToArray(List<List<UInt64>> PrimesList, string fullDocPath)
         {
             Console.WriteLine("Now starting the process of converting strings from the file into UInt64s.\n");
+
+            List<List<UInt64>> PrimesListTemp = new List<List<UInt64>>();
 
             List<UInt64> Primes = new List<UInt64>();
             List<UInt64> Primes2 = new List<UInt64>();
             List<UInt64> Primes3 = new List<UInt64>();
             List<UInt64> Primes4 = new List<UInt64>();
             List<UInt64> Primes5 = new List<UInt64>();
+            List<UInt64> Primes6 = new List<UInt64>();
+            List<UInt64> Primes7 = new List<UInt64>();
+            List<UInt64> Primes8 = new List<UInt64>();
+            List<UInt64> Primes9 = new List<UInt64>();
+            List<UInt64> Primes10 = new List<UInt64>();
+            List<UInt64> Primes11 = new List<UInt64>();
+            List<UInt64> Primes12 = new List<UInt64>();
+            List<UInt64> Primes13 = new List<UInt64>();
+            List<UInt64> Primes14 = new List<UInt64>();
+            List<UInt64> Primes15 = new List<UInt64>();
+            List<UInt64> Primes16 = new List<UInt64>();
+            List<UInt64> Primes17 = new List<UInt64>();
+            List<UInt64> Primes18 = new List<UInt64>();
+            List<UInt64> Primes19 = new List<UInt64>();
+            List<UInt64> Primes20 = new List<UInt64>();
+            List<UInt64> Primes21 = new List<UInt64>();
+            List<UInt64> Primes22 = new List<UInt64>();
+            List<UInt64> Primes23 = new List<UInt64>();
+            List<UInt64> Primes24 = new List<UInt64>();
+            List<UInt64> Primes25 = new List<UInt64>();
+            List<UInt64> Primes26 = new List<UInt64>();
+            List<UInt64> Primes27 = new List<UInt64>();
+            List<UInt64> Primes28 = new List<UInt64>();
+            List<UInt64> Primes29 = new List<UInt64>();
+            List<UInt64> Primes30 = new List<UInt64>();
 
-            PrimesList.Add(Primes);
-            PrimesList.Add(Primes2);
-            PrimesList.Add(Primes3);
-            PrimesList.Add(Primes4);
-            PrimesList.Add(Primes5);
+            PrimesListTemp.Add(Primes);
+            PrimesListTemp.Add(Primes2);
+            PrimesListTemp.Add(Primes3);
+            PrimesListTemp.Add(Primes4);
+            PrimesListTemp.Add(Primes5);
+            PrimesListTemp.Add(Primes6);
+            PrimesListTemp.Add(Primes7);
+            PrimesListTemp.Add(Primes8);
+            PrimesListTemp.Add(Primes9);
+            PrimesListTemp.Add(Primes10);
+            PrimesListTemp.Add(Primes11);
+            PrimesListTemp.Add(Primes12);
+            PrimesListTemp.Add(Primes13);
+            PrimesListTemp.Add(Primes14);
+            PrimesListTemp.Add(Primes15);
+            PrimesListTemp.Add(Primes16);
+            PrimesListTemp.Add(Primes17);
+            PrimesListTemp.Add(Primes18);
+            PrimesListTemp.Add(Primes19);
+            PrimesListTemp.Add(Primes20);
+            PrimesListTemp.Add(Primes21);
+            PrimesListTemp.Add(Primes22);
+            PrimesListTemp.Add(Primes23);
+            PrimesListTemp.Add(Primes24);
+            PrimesListTemp.Add(Primes25);
+            PrimesListTemp.Add(Primes26);
+            PrimesListTemp.Add(Primes27);
+            PrimesListTemp.Add(Primes28);
+            PrimesListTemp.Add(Primes29);
+            PrimesListTemp.Add(Primes30);
 
-            string[] lines;
+            string[] lines = new string[100000000 / 2];
+            bool temp = true;
 
-            for (int pos = 0; pos < PrimesList.Count; pos++)
+            for (int pos = 0; pos < PrimesListTemp.Count && temp; pos++)
             {
+                PrimesList.Add(PrimesListTemp[pos]);
                 lines = File.ReadLines(fullDocPath).Skip(MaxArrSize * pos).Take(MaxArrSize).ToArray();
 
                 foreach (string line in lines)
@@ -43,7 +97,23 @@ namespace PrimeToFile
                     }
                 }
                 Console.WriteLine("PrimesList[" + pos + "] was just imported");
+
+                if (pos > 0)
+                {
+                    try
+                    {
+                        if (PrimesList[pos].First() == PrimesList[pos-1].First())
+                        {
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                        break;
+                    }
+                }
             }
+            Array.Clear(lines, 0, lines.Length);
         }
 
         public static void FindFolder(string fullDocPath)
